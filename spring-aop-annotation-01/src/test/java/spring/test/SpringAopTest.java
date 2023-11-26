66027992bb49c7d3329d538ae5c5fcd3a9939551
gxz_pc <xizhuogao@foxmail.com>
2023-11-26 11:47:02 +0800

    P56 over！

--- spring-aop-annotation-01/src/test/java/spring/test/SpringAopTest.java
+++ spring-aop-annotation-01/src/test/java/spring/test/SpringAopTest.java
@@ -10,9 +10,9 @@ import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 public class SpringAopTest {
     @Autowired
     private Calculator calculator;
-    //不能用实现类接值,只能使用父接口,
+    //1.当实现类实现了接口时，不能用实现类接值,只能使用父接口,
     //因为Aop底层默认使用jdk动态代理生成:采用代理对象和目标对象实现同一个接口的方式
-    //当实现类没有实现接口时,使用cglib动态代理生成:采用代理对象是目标对象子类的方式
+    //2.当实现类没有实现接口时,使用cglib动态代理生成:采用代理对象是目标对象子类的方式
     @Test
     public void test(){
         int add= calculator.add(3,4);
